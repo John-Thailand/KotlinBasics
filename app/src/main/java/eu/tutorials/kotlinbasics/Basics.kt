f0aa2ecfccd662bb1d36479d0ad55c6f9b39a1b8
John-Thailand <naoki.honda@mirrorfit.jp>
2024-06-20 21:19:57 +0900

    オブジェクト生成時にinitが実行される

--- app/src/main/java/eu/tutorials/kotlinbasics/Basics.kt
+++ app/src/main/java/eu/tutorials/kotlinbasics/Basics.kt
@@ -1,15 +1,5 @@
 package eu.tutorials.kotlinbasics
 
 fun main() {
-    var myResult = add(5, 10)
-    println(myResult)
-}
-
-fun add(num1: Int, num2: Int): Int {
-    val result = num1 + num2
-    return result
-}
-
-fun makeCoffee(sugarCount: Int) {
-    println("Coffee with $sugarCount spoons of sugar")
+    var daisy = Dog()
 }
---
+++ app/src/main/java/eu/tutorials/kotlinbasics/Dog.kt
@@ -0,0 +1,11 @@
+package eu.tutorials.kotlinbasics
+
+class Dog {
+    init {
+        bark()
+    }
+
+    fun bark() {
+        println("Woof Woof")
+    }
+}
